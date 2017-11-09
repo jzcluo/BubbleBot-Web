dd7aaa7354a9a18f39c5165cab1aab1ccd5f2952
JLuo <jeffreyzcluo@gmail.com>
2017-11-08 20:41:19 -0500

    testing server

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello");
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.listen(process.env.PORT || 3000);
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+app.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
