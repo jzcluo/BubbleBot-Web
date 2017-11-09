9af8ce87a79962e976bb5c528719f4cc31b1b972
JLuo <jeffreyzcluo@gmail.com>
2017-11-08 19:24:45 -0500

    testing server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end("hello");
+    res.send("hello");
 });
 
 app.listen(process.env.PORT || 1337);
